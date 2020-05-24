5cc836a7e646dc32fafb810063b4528c26c0b90b
AdityaSingh17 <adityarsingh17@gmail.com>
2020-05-24 11:43:54 +0530

    Added insert rear and insert specified functions

--- sllvisualizer.cpp
+++ sllvisualizer.cpp
@@ -63,6 +63,60 @@ NODE  insert_front(int item, NODE first)
     return temp;
 }
 
+// Insert element from Rear of Linked list.
+NODE insert_rear(int item, NODE first)
+{
+    NODE temp;   
+    NODE cur;
+    temp = getnode();
+    temp->info = item;
+    temp->link = NULL;
+    if(first == NULL)
+    return temp;
+    cur = first;
+    while(cur->link != NULL)
+        cur = cur->link;
+    cur->link = temp;
+    return first;
+}
+
+// Insert node at a specified position.
+NODE insert_specified(int item, NODE first)
+{ 
+    NODE next, cur, temp;
+    temp = getnode();
+    temp->info = item;
+    if(first == NULL)
+    {
+        printf("The list is empty! Inserting node at the front...\n");
+        temp->link = NULL;
+        first = temp;
+        return first;
+    }
+    printf("Enter the key : "); // Key = Node value after which the new node is to be inserted.
+    scanf("%d", &key);
+    cur = first;       
+    while(cur != NULL)
+    {
+        next = cur->link;
+        if(cur->info == key)
+        {
+            printf("Key found!\n");
+            cur->link = temp;
+            temp->link = next;
+            return first;
+        }
+            cur = cur->link;
+    }
+   if(cur == NULL)
+    {
+        printf("Search unsuccessful!\n");
+        count--;
+        ins_succ = 0;
+    }
+    return first;
+}
+
 // Action on option.
 void option(int choice)
 {
@@ -76,9 +130,25 @@ void option(int choice)
             lldisplay();
             printf("NEW NODE INSERTED\n");
             break;
-        case 2: // TODO
+        case 2:
+            printf("Enter the item to be inserted: ");
+            scanf("%d", &item);
+            first = insert_rear(item, first);
+            count++;
+            lldisplay();
+            printf("NEW NODE INSERTED\n");
             break;
-        case 3: // TODO
+        case 3:
+            printf("Enter the item to be inserted: ");
+            scanf("%d", &item); 
+            ins_succ = 1;
+            first = insert_specified(item, first);
+            count++;
+            lldisplay();
+            if(!ins_succ)
+                printf("KEY NODE DOESN'T EXIST\n");
+            else
+                printf("NEW NODE INSERTED\n");
             break;
         case 4: // TODO
             break;
@@ -100,9 +170,9 @@ int main()
     while(1)
     {
         printf("\n1. Insert Front");
-        /* TODO
         printf("\n2. Insert Rear");
         printf("\n3. Insert Specified");
+        /* TODO
         printf("\n4. Delete Front");
         printf("\n5. Delete Rear");
         printf("\n6. Delete Specified");
